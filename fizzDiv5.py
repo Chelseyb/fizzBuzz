fa84ed703cfb57f79bb4f309b42c538df6d9c5de
Chelseyb <54458948+Chelseyb@users.noreply.github.com>
2021-10-15 17:01:12 -0500

    Add files via upload

---
+++ fizzDiv5.py
@@ -0,0 +1,13 @@
+# Fizz buzz branch 2 (divide by 3)
+print("Fizzbuzz challenge")
+
+
+for numslist in range(101):
+
+    
+    if numslist % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(numslist)
+
