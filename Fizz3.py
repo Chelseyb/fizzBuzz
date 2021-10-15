8f8a9efde808f82c6515edcffae67d4f5ca3059c
Chelseyb <54458948+Chelseyb@users.noreply.github.com>
2021-10-15 17:00:13 -0500

    Add files via upload

---
+++ Fizz3.py
@@ -0,0 +1,13 @@
+# Fizz buzz brach 1 (divide by 3)
+print("Fizzbuzz challenge")
+
+
+for numslist in range(101):
+
+    
+    if numslist % 3 == 0:
+        print("Fizz")
+
+    else:
+        print(numslist)
+
